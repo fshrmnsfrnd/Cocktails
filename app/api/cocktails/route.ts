ff58658d69ed163570ea9d6d648f36aba0a75858
fshrmnsfrnd <fshrmnsfrnd@web.de>
2025-11-21 14:59:28 +0100

    sortierung geändert

--- app/api/cocktails/route.ts
+++ app/api/cocktails/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 
 export async function GET() {
-    const cocktails = await db.all("SELECT Cocktail_ID, Name, Description FROM Cocktail");
+    const cocktails = await db.all("SELECT Cocktail_ID, Name, Description FROM Cocktail ORDER BY Name");
     return NextResponse.json(cocktails)
 }
